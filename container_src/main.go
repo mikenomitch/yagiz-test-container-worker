759ad408bccdf9795a5d60a4a7e72ce8a45196ef
Cody Roseborough <croseborough@cloudflare.com>
2025-03-14 12:58:07 -0700

    Fix some minor format issues

--- container_src/main.go
+++ container_src/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    country := os.Getenv("CLOUDFLARE_COUNTRY_A2")
-    location := os.Getenv("CLOUDFLARE_LOCATION")
-    region :=os.Getenv("CLOUDFLARE_REGION")
+	country := os.Getenv("CLOUDFLARE_COUNTRY_A2")
+	location := os.Getenv("CLOUDFLARE_LOCATION")
+	region := os.Getenv("CLOUDFLARE_REGION")
 
-    fmt.Fprintf(w, "Hi, I'm a container running in %s, %s, which is part of %s ", location, country, region)
+	fmt.Fprintf(w, "Hi, I'm a container running in %s, %s, which is part of %s ", location, country, region)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
